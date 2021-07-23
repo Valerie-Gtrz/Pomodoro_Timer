6f0c80811348929edb9eacc263bed5405ef88b8f
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-07-23 15:41:40 -0500

    update msg to format time

--- src/pomodoro/Pomodoro.js
+++ src/pomodoro/Pomodoro.js
@@ -73,7 +73,7 @@ function Pomodoro() {
       }
       return setSession(nextTick);
     },
-    isTimerRunning ? 1000 : null
+    isTimerRunning ? 10 : null
   );
 
   /**
@@ -224,7 +224,7 @@ function Pomodoro() {
           </div>
         </div>
       </div>
-      <Session session={session} />
+      <Session session={session} focusDuration={focusDuration} breakDuration={breakDuration}/>
     </div>
   );
 }
--- src/pomodoro/Session.js
+++ src/pomodoro/Session.js
@@ -1,19 +1,20 @@
 import React from "react";
+import { minutesToDuration, secondsToDuration } from "../utils/duration";
 
-function Session({ session }) {
+function Session({ session, breakDuration, focusDuration }) {
   return (
     session && (
       <div>
-        {/* TODO: This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
+        {/* DONE! This area should show only when there is an active focus or break - i.e. the session is running or is paused */}
         <div className="row mb-2">
           <div className="col">
             {/* TODO: Update message below to include current session (Focusing or On Break) total duration */}
             <h2 data-testid="session-title">
-              {session?.label} for 25:00 minutes
+              {session.label} for {session.label === "Focusing" ? minutesToDuration(focusDuration): minutesToDuration(breakDuration)}
             </h2>
             {/* TODO: Update message below correctly format the time remaining in the current session */}
             <p className="lead" data-testid="session-sub-title">
-              {session?.timeRemaining} remaining
+              {secondsToDuration(session.timeRemaining)} remaining
             </p>
           </div>
         </div>
