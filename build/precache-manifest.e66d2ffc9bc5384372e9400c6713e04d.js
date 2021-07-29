92bda7c0bbb124d30a4f9d1a3482174c6cdf7901
Valerie Gutierrez <Valeriengutierrez@yahoo.com>
2021-07-29 12:11:40 -0500

    Add react app

---
+++ build/precache-manifest.e66d2ffc9bc5384372e9400c6713e04d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee18f02c79c9e80cd4f6107abbbca903",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f61f875169a7ab443bc6",
+    "url": "/static/css/main.831ed7db.chunk.css"
+  },
+  {
+    "revision": "8f60e831c49dd590640b",
+    "url": "/static/js/2.0acf020f.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/static/js/2.0acf020f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f61f875169a7ab443bc6",
+    "url": "/static/js/main.cfdbd5df.chunk.js"
+  },
+  {
+    "revision": "facd31b36309f08ff9ad",
+    "url": "/static/js/runtime-main.acde468f.js"
+  }
+]);
